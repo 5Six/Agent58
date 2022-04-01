d039d53a7105cada85940cacdffde57e5aa1199b
Warren Yip <shy35@bath.ac.uk>
2022-04-01 12:32:44 +0100

    render

--- src/main.py
+++ src/main.py
@@ -4,4 +4,12 @@ import ale_py
 print('gym:', gym.__version__)
 print('ale_py:', ale_py.__version__)
 
-env = gym.make('ALE/Boxing-ram-v5')
+env = gym.make('ALE/Boxing-ram-v5', render_mode='human')
+env.reset()
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+
+    if done:
+        observation, info = env.reset(return_info=True)
+env.close()
