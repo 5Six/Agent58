dcbbc1465fa393e09faaa730c44fede2c7bf578f
Andre Lim <andrelyk777ace@gmail.com>
2022-04-10 14:47:08 +0100

    import pytorch and init replay mem

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import gym
 import ale_py
+import torch
+from collections import deque
 
 print('gym:', gym.__version__)
 print('ale_py:', ale_py.__version__)
 
-env = gym.make('ALE/Boxing-ram-v5', render_mode='human')
+env = gym.make('Boxing-ram-v4', render_mode='human')
 env.reset()
-for _ in range(1000):
+
+capacity = 1000
+replay = deque([], capacity)
+
+while not done:
     action = env.action_space.sample()
     observation, reward, done, info = env.step(action)
 
     if done:
         observation, info = env.reset(return_info=True)
+    
+
 env.close()
