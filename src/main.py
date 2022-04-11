97f6596b464396c6a3bcc80683cf9cb9ba1d4211
Oguz Tec <tecirliogluoguz@gmail.com>
2022-04-11 12:38:55 +0100

    Began work on structure of training loop

--- src/main.py
+++ src/main.py
@@ -9,9 +9,33 @@ print('ale_py:', ale_py.__version__)
 env = gym.make('ALE/Boxing-ram-v5', render_mode='human')
 env.reset()
 
-done = 0
-
 flag = 0
+
+SEED = 5
+ALPHA = 0.2
+GAMMA = 0.9
+EPSILON = 0.15
+EPISODES = 150
+AGENTS = 2
+PLANNING_STEPS = 5
+MIN_EPSILON = 0.0
+MIN_EPISODE_DECAY = 5
+
+
+
+TOTAL_EPISODE_COUNT = 100
+i = 0
+
+while i < TOTAL_EPISODE_COUNT:
+    done = False
+    time_step = 1
+    #init state S1
+    while not done:
+        
+
+
+        time_step += 1 
+    i+=1
 
 while not done:
     action = 1
@@ -24,6 +48,6 @@ while not done:
 
     if done:
         observation, info = env.reset(return_info=True)
-    
+   
 
 env.close()
