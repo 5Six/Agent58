5a157a7ff2a40741fdfc263b7732f55e1ae48e44
Andre Lim <andrelyk777ace@gmail.com>
2022-04-11 15:23:47 +0100

    andre's dqn

--- src/main.py
+++ src/main.py
@@ -1,29 +1,60 @@
+from dqn_agent import DQN_Agent
 import gym
-import ale_py
-import Replay
-import DQN
+from tqdm import tqdm
+import torch
 
-print('gym:', gym.__version__)
-print('ale_py:', ale_py.__version__)
+# # if gpu is to be used
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-env = gym.make('ALE/Boxing-ram-v5', render_mode='human')
-env.reset()
+env = gym.make('ALE/Boxing-ram-v5')
+input_dim = env.observation_space.shape[0]
+output_dim = env.action_space.n
+exp_replay_size = 256
+agent = DQN_Agent(seed=1423, layer_sizes=[input_dim, 64, output_dim], lr=1e-3, sync_freq=5,
+                  exp_replay_size=exp_replay_size)
 
-done = 0
+# Main training loop
+losses_list, reward_list, episode_len_list, epsilon_list = [], [], [], []
+episodes = 10000
+epsilon = 1
 
-flag = 0
+# initiliaze experiance replay
+index = 0
+for i in range(exp_replay_size):
+    obs = env.reset()
+    done = False
+    while not done:
+        A = agent.get_action(obs, env.action_space.n, epsilon=1)
+        obs_next, reward, done, _ = env.step(A.item())
+        agent.collect_experience([obs, A.item(), reward, obs_next])
+        obs = obs_next
+        index += 1
+        if index > exp_replay_size:
+            break
 
-while not done:
-    action = 1
-    state, reward, done, info = env.step(action)
-    # print(env.get_keys_to_action())
-    print(env.get_action_meanings())
-    if flag == 0:
-        print(info)
-        flag = 1
+index = 128
+for i in tqdm(range(episodes)):
+    obs, done, losses, ep_len, rew = env.reset(), False, 0, 0, 0
+    while not done:
+        ep_len += 1
+        A = agent.get_action(obs, env.action_space.n, epsilon)
+        obs_next, reward, done, _ = env.step(A.item())
+        agent.collect_experience([obs, A.item(), reward, obs_next])
 
-    if done:
-        observation, info = env.reset(return_info=True)
-    
+        obs = obs_next
+        rew += reward
+        index += 1
 
-env.close()
+        if index > 128:
+            index = 0
+            for j in range(4):
+                loss = agent.train(batch_size=16)
+                losses += loss
+    if epsilon > 0.05:
+        epsilon -= (1 / 5000)
+
+    losses_list.append(losses / ep_len), reward_list.append(rew)
+    episode_len_list.append(ep_len), epsilon_list.append(epsilon)
+
+print("Saving trained model")
+agent.save_trained_model("boxing-dqn.pth")
