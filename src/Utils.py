21b764b3461e4a7c10aa80319fbc7e9b7687c16d
Lawrence Wong <lawrencedrums@gmail.com>
2022-04-24 22:03:37 +0100

    Created Plot class

--- src/Utils.py
+++ src/Utils.py
@@ -1,5 +1,7 @@
+import os
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_data(path: str) -> None:
@@ -29,3 +31,27 @@ def nparray_to_tensor(nparray, device: torch.device = torch.device("cpu")) -> to
 #     if is_ipython:
 #         display.clear_output(wait=True)
 #         display.display(plt.gcf())
+
+class Plot:
+    def __init__(self, method='', custom_name=''):
+        self.x_label = "Episodes"
+        self.y_label = "Last 100 average score"
+        self.plot_save_path = self.get_save_path(method, custom_name)
+
+    def get_plot(self, avg_score_history):
+        plt.close("all")
+        plt.plot(avg_score_history)
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
+        plt.savefig(self.plot_save_path)
+
+    def get_save_path(self, method, custom_name):
+        if custom_name:
+            custom_name = "_" + custom_name
+        save_path = f"plot/plot_boxing-v5_{method}DQN{custom_name}"
+
+        i = 1
+        while os.path.exists(f"{save_path}_{i}.png"):
+            i += 1
+        
+        return f"{save_path}_{i}.png"
