f6471d7be0318b480617271f829fea2a239a2c45
Andre Lim <andrelyk777ace@gmail.com>
2022-04-25 17:03:04 +0100

    dueling network working for refactored code, performance is not as good as double

--- src/Net.py
+++ src/Net.py
@@ -14,3 +14,31 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         return x
+
+class Dueling_DQN(nn.Module):
+    def __init__(self, dimension: int, output: int) -> None:
+        super(Dueling_DQN, self).__init__()
+        self.fc1_adv = nn.Linear(256, 256)
+        self.fc1_val = nn.Linear(256, 256)
+
+        self.fc2_adv = nn.Linear(256, 256)
+        self.fc2_val = nn.Linear(256, 256)
+
+        self.fc3_adv = nn.Linear(256, 18)
+        self.fc3_val = nn.Linear(256, 1)
+
+
+    def forward(self, x):   
+        adv = F.relu(self.fc1_adv(x))
+        val = F.relu(self.fc1_val(x))
+
+        adv = F.relu(self.fc2_adv(adv))
+        val = F.relu(self.fc2_val(val))
+
+        adv = self.fc3_adv(adv)
+        # val = self.fc3_val(val).expand(x.size(0), 18)
+        val = self.fc3_val(val)
+
+        # q = val + (adv - adv.mean(1).unsqueeze(1).expand(x.size(0), 18))
+        q = val + (adv - adv.mean())
+        return q
