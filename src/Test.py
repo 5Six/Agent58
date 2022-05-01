d1016a6215361c9438ee3cbf8da3e4ee3deaf173
ThomasPoultney <80142772+ThomasPoultney@users.noreply.github.com>
2022-05-01 13:27:26 +0100

    trained PER and Double

--- src/Test.py
+++ src/Test.py
@@ -11,8 +11,8 @@ def test():
 	using_per = ""
 	using_dueling = False
     
-	net_path = f"net/net_boxing-v5_{method}DQN_{custom_name}{using_per}_action_net.pth"
-	#net_path = "net/net_boxing-v5_doubleDQN_double_3000_episode_final_1_action_net.pth"
+	#net_path = f"net/net_boxing-v5_{method}DQN_{custom_name}{using_per}_action_net.pth"
+	net_path = "./net/net_boxing-v5_doubleDQN_PER_test_1_action.pth"
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 	
